9fcf51e2a99d67fa53675e597a2a84c030c4155a
Ahmadalkhayat <Ahmadmalkhayay66@gmail.com>
2023-03-12 21:37:27 +0200

    commit

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -6,24 +6,24 @@ const [count,setcount]=useState(false);
     return(
         <div className="counter ">
         <div className="box text-center">
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-6">
             <i class="fa-sharp fa-solid fa-paint-roller pb-4 fs-1"></i>
             <ScrollTrigger onEnter={()=>{setcount(true)}} onExit={()=>{setcount(false)}}>
             <h2>
             {count && <CountUp start={0} end={100} duration={2} delay={0} /> }+ </h2>
             </ScrollTrigger>
 
-            <p className="fs-3 fw-bold pt-2" >Satisfied Clients</p>
+            <p className="fs-3 fw-bold pt-2 mb-3" >Satisfied Clients</p>
             </div>
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-6">
             <i class="fa-solid fa-brush fs-1 pb-4"></i>
             <ScrollTrigger onEnter={()=>{setcount(true)}} onExit={()=>{setcount(false)}}>
             <h2>
             {count && <CountUp start={250} end={500} duration={2} delay={0} /> }K </h2>
             </ScrollTrigger>
-            <p className="fs-3 fw-bold pt-2" >Active Clients</p>
+            <p className="fs-3 fw-bold pt-2 pt-2 mb-3" >Active Clients</p>
             </div>
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-6">
             <i class="fa-solid fa-palette fs-1 pb-4 "></i>
             <ScrollTrigger onEnter={()=>{setcount(true)}} onExit={()=>{setcount(false)}}>
             <h2>
@@ -31,7 +31,7 @@ const [count,setcount]=useState(false);
             </ScrollTrigger>
             <p  className="fs-3 fw-bold pt-2" >Industry Solutions</p>
             </div>
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-6">
             <i class="fa-solid fa-paintbrush fs-1 pb-4"></i>
             <ScrollTrigger onEnter={()=>{setcount(true)}} onExit={()=>{setcount(false)}}>
             <h2>
